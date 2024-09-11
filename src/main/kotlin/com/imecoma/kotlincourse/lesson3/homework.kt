c5bc708f62c70185ece17e83eac2fdbe0d868dd7
carmich <aliseinwonderland90@bk.ru>
2024-09-12 02:00:14 +0300

    initial message

--- src/main/kotlin/com/imecoma/kotlincourse/lesson3/homework.kt
+++ src/main/kotlin/com/imecoma/kotlincourse/lesson3/homework.kt
@@ -4,3 +4,16 @@ class homework {
     //
 }
 
+val event: String = "Pivozavr fest 2024" //название forever, неизменно
+var data: String = "15.09.2024" //дата м.б измнена
+var place: String = "Krasnodar, Krasnaya Street, shopping center gallery" // место проведения так же может быть изменено
+//расходы на оборудование, утверждены и неизменны
+val beerBottlingEquipmentRental: Double = 50000.0 //аренда пивного оборудования
+val furnitureRental: Double = 20000.0 //аренда мебели
+//расходы на кейтеринг, утверждены и неизменны
+val catering: Double = 30000.0 //кейтеринг
+//прочие расходы, утверждены и неизменны
+val rentRoom: Double = 30000.0//аренда помещения
+val security: Double = 5000.0 // охрана
+val decoration: Double = 5000.0//декор, дизаин
+
