a32031ed7c79a6b0cf733a0432f9da7f57f7c991
carmich <aliseinwonderland90@bk.ru>
2024-09-18 18:09:48 +0300

    initial message

---
+++ src/main/kotlin/les5/les5.1.kt
@@ -0,0 +1,24 @@
+package les5
+
+fun main () {
+    val a =(3 + 2 < 6) && (4 * 2 == 8)// true = true - true
+    (10 - 5 >= 5) || (2 * 3 != 6)// true = false - true
+    (8 / 2 == 4) && (7 % 3 != 1)// true = false - false
+    (9 - 3 >= 6) && (8/2 != 4)// true = false - false
+    ((3 + 4) < 8) && (12 / 3 == 4) || (5 % 2 != 1)// true = true = false - true
+    ((18 - 5) >= 5) || (6 * 2 != 12) && !(9 /3 > 2)// true = false = false - true
+    ((2 * 5) == 10) && !(7 - 3 < 5) || (8 / 2 == 4)// true = false = true - true
+    ((9 + 2) < 12) && (15 % 4 != 3) || !(4 * 2 == 8)// true = false = false - false
+
+    ((var a = 4; a-- == 4) || (5 * 2 != 10)) && (3 + 1 == 4)// true = false = true - true
+    (var b = 3; b++ > 3) && (6 / 2 == 3) || (7 - 2 != 5)// false = true = false - false
+    (var c = 1; ++c < 3) || ((4 % 2 == 0) && (5 + 0 == 5))// true = true = true - true
+    ((var d = 5; d-- = 5) && (8 / 4 == 2)) || (2 * 3 != 6)// true = true = false - true
+    (var e = 2; e++ < 3) && (7 + 1 == 8) || (4 % 2 != 0)// true = true = false - true
+
+
+
+
+    println (a)
+}
+
